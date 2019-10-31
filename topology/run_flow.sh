f94043868a12a1c98287436c3332ed9a61f48ff5
Patrick Bryant <pbryant@b-an01.hpc2n.umu.se>
2019-10-31 14:41:58 +0100

    creating topology dataset

--- topology/run_flow.sh
+++ topology/run_flow.sh
@@ -14,23 +14,24 @@ GITDIR=/home/p/pbryant/pfs/evolutionary_rates
 #Filter the clustered sequences according to experimental method used for
 #structural determination and X-ray resolution
 CLUSTERED_SEQUENCES=$GITDIR'/clustering/reallybelow95.fa' #Clustered sequences
-RESULTS_DIR=/home/p/pbryant/pfs/runs/CATH/test/20191026 #RESULTS DIRECTORY
+RESULTS_DIR=/home/p/pbryant/pfs/runs/CATH/test/20191031 #RESULTS DIRECTORY
 
-#$GITDIR/pdb_filter.py $CLUSTERED_SEQUENCES $GITDIR/Xray_res2.6Å.txt $RESULTS_DIR/
+$GITDIR/pdb_filter.py $CLUSTERED_SEQUENCES $GITDIR/Xray_res2.6Å.txt $RESULTS_DIR/
 #wait
 #Select H-groups with at least two sequences and group them according to their H-group
 HGROUPS=$GITDIR'/H_groups.tsv'
 SEQUENCES=$CLUSTERED_SEQUENCES
-FAILED_PDB_FILTER=$RESULTS_DIR'/failed_pdb_filter'
+FAILED_PDB_FILTER=$RESULTS_DIR'/failed_pdb_filter.txt'
 FASTADIR=/home/p/pbryant/pfs/data/CATH/complete_flow/topology #where to write the grouped fasta sequnces
 singularity exec /pfs/nobackup/home/p/pbryant/singularity/bio.sif $GITDIR/topology/same_topology.py --H_groups $HGROUPS --sequences $SEQUENCES --failed_pdb_filter $FAILED_PDB_FILTER --outdir $FASTADIR/ --logfile $LOGFILE
 
 
 #Get data, make alignments and calculate scores
 #best done in parallel
+#Make sure all directories in all runscripts match the current run
 #wait
-#$GITDIR/get_parallel.sh
+$GITDIR/get_parallel.sh
 
-#wait
+wait
 #Get all results into a unified df
-#$GITDIR/merge_results.py
+$GITDIR/merge_results.py
