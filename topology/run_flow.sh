eeb80dc04261ec3395f53194656312215129fa07
Patrick Bryant <pbryant@b-an01.hpc2n.umu.se>
2019-10-31 13:35:50 +0100

    added logfile

--- topology/run_flow.sh
+++ topology/run_flow.sh
@@ -1,31 +1,34 @@
 #!/bin/bash -l
 
 
-#A bash script for getting CATH files for a certain H-group.
+#A bash script for getting CATH files for a certain topology.
 #Sequence and structural alignments are then performed.
 #Different metrics are thereafter calculated: secondary structure,
 #surface accessibility, RMSD, Maximum likelihood amino acid distance,
 #TMscore, LDDT-score
 
+#Log file
+LOGFILE=logs.txt
 #Path to git directory
 GITDIR=/home/p/pbryant/pfs/evolutionary_rates
 #Filter the clustered sequences according to experimental method used for
 #structural determination and X-ray resolution
-CLUSTERED_SEQUENCES=$GITDIR'/clustering/cath-domain-seqs-0.2_rep_seq.fasta' #Clustered sequences
-RESULTS_DIR=/home/p/pbryant/pfs/results/CATH/20191017 #RESULTS DIRECTORY
-$GITDIR/pdb_filter.py $CLUSTERED_SEQUENCES $GITDIR/Xray_res2.6Å.txt $RESULTS_DIR/
-wait
+CLUSTERED_SEQUENCES=$GITDIR'/clustering/reallybelow95.fa' #Clustered sequences
+RESULTS_DIR=/home/p/pbryant/pfs/runs/CATH/test/20191026 #RESULTS DIRECTORY
+
+#$GITDIR/pdb_filter.py $CLUSTERED_SEQUENCES $GITDIR/Xray_res2.6Å.txt $RESULTS_DIR/
+#wait
 #Select H-groups with at least two sequences and group them according to their H-group
 HGROUPS=$GITDIR'/H_groups.tsv'
-SEQUENCES=$GITDIR'/clustering/cath-domain-seqs-0.2_rep_seq.fasta'
+SEQUENCES=$CLUSTERED_SEQUENCES
 FAILED_PDB_FILTER=$RESULTS_DIR'/failed_pdb_filter'
-FASTADIR=/home/p/pbryant/pfs/data/CATH/complete_flow/distant #where to write the grouped fasta sequnces
-singularity exec /pfs/nobackup/home/p/pbryant/singularity/bio.sif $GITDIR/above2.py --H_groups $HGROUPS --sequences $SEQUENCES --failed_pdb_filter $FAILED_PDB_FILTER --outdir $FASTADIR/
+FASTADIR=/home/p/pbryant/pfs/data/CATH/complete_flow/topology #where to write the grouped fasta sequnces
+singularity exec /pfs/nobackup/home/p/pbryant/singularity/bio.sif $GITDIR/topology/same_topology.py --H_groups $HGROUPS --sequences $SEQUENCES --failed_pdb_filter $FAILED_PDB_FILTER --outdir $FASTADIR/ --logfile $LOGFILE
 
 
 #Get data, make alignments and calculate scores
 #best done in parallel
-wait
+#wait
 #$GITDIR/get_parallel.sh
 
 #wait
--- topology/same_topology.py
+++ topology/same_topology.py
@@ -5,9 +5,11 @@
 import argparse
 import sys
 import pandas as pd
-import pdb
+import random
 import os
+import numpy as np
 
+import pdb
 #Arguments for argparse module:
 parser = argparse.ArgumentParser(description = '''A program that filters entries in CATH
         according to a filter file containing PDB ids. All entries in each topology are then 
@@ -22,7 +24,8 @@ parser.add_argument('--failed_pdb_filter', nargs=1, type= str,
                   default=sys.stdin, help = 'path to uids that failed the pdb filter.')
 parser.add_argument('--outdir', nargs=1, type= str,
                   default=sys.stdin, help = 'path output directory.')
-
+parser.add_argument('--logfile', nargs=1, type= str,
+                  default=sys.stdin, help = 'logfile for stats.')
 
 
 def read_tsv(H_groups_file):
@@ -81,7 +84,7 @@ def get_groups(topologies, sequences):
 	return grouped_sequences
 
 
-def get_passed_uids(failed_pdb_filter, sequences, topologies):
+def get_passed_uids(failed_pdb_filter, sequences, topologies, num_topologies_before):
     '''Get the uids that passed the pdb filter and group them in topologies
     '''
 
@@ -103,7 +106,17 @@ def get_passed_uids(failed_pdb_filter, sequences, topologies):
         else:
             passed_uids_grouped[topology].append(uid) #Otherwise append
 
+    with open(logfile, 'a+') as file: #append to file and create it if it does not exist
+        file.write('PDB filter\n')
+        file.write('#uids before:'+str(len(sequences))+'\t')
+        file.write('#uids after:'+str(len(passed_uids))+'\t')
+        file.write('%uids passed:'+str(np.around(len(passed_uids)/len(sequences)*100, decimals = 1))+'\n')
+        file.write('#Topologies before:'+str(num_topologies_before)+'\t')
+        file.write('#Topologies after:'+str(len(passed_uids_grouped))+'\t')
+        file.write('%Topologies passed:'+str(np.around(len(passed_uids_grouped)/num_topologies_before*100, decimals = 1))+'\n')
 
+	
+	
     return passed_uids_grouped
 
 def overx(passed_uids_grouped, outdir, sequences, H_groups):
@@ -116,26 +129,29 @@ def overx(passed_uids_grouped, outdir, sequences, H_groups):
 
 	x=2 #At least two entries per topology for a pair
 	#write passed_uids_grouped_over_x to fasta
-	pdb.set_trace()
 	for group in passed_uids_grouped:
 		group_dir = outdir+'fasta/'+group
 		uids = passed_uids_grouped[group]
-		if len(uids)<2:
+		if len(uids)<2: #Need to uids to make a pair
 			continue
-		os.mkdir(group_dir)
-		topology_groupings.write(subgroup_dir+'\n')
 		#shuffle uids to make sure the groupings are random
 		random.Random(2).shuffle(uids)
 		used_hgroups = [] #Save one uid per H-group
+		selected_uids = [] #The chosen uids
 		for uid in uids:
 			hgroup = H_groups[uid]
-			pdb.set_trace()
 			if hgroup in used_hgroups:#Only use each h-group once
 				continue
 			else:
 				used_hgroups.append(hgroup)
+				selected_uids.append(uid)
+		if len(selected_uids)<2: #If there is not at least one pair
+			continue
+		os.mkdir(group_dir)
+		topology_groupings.write(group_dir+'\n')
+		for uid in selected_uids:
 			sequence = sequences[uid]
-			with open(subgroup_dir+'/'+uid+'.fa', "w") as file:
+			with open(group_dir+'/'+uid+'.fa', "w") as file:
 				file.write('>'+uid+'\n')
 				j = 0 #index
 				while j<len(sequence):
@@ -152,6 +168,10 @@ H_groups, topologies = read_tsv(args.H_groups[0]) #Read H-groups and uids
 sequences = read_fasta(args.sequences[0]) #Read the clustered sequences
 failed_pdb_filter = pd.read_csv(args.failed_pdb_filter[0], sep = '\n', header = None) #Get uids that failed the pdb filter
 outdir = args.outdir[0]
+logfile = args.logfile[0]
 
-passed_uids_grouped = get_passed_uids(failed_pdb_filter, sequences, topologies) #Get uids that passed the pdb filter and group them
+#Number of topologies before
+grouped_seqs_by_topology = get_groups(topologies, sequences)
+#Assess which passed
+passed_uids_grouped = get_passed_uids(failed_pdb_filter, sequences, topologies, len(grouped_seqs_by_topology)) #Get uids that passed the pdb filter and group them
 overx(passed_uids_grouped, outdir, sequences, H_groups)
