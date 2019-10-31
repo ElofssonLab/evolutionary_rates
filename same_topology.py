f1b28f2864979193a1a60a770c8e00a1799513be
Patrick Bryant <pbryant@b-an01.hpc2n.umu.se>
2019-10-31 10:02:35 +0100

    using one entry per H-group

--- same_topology.py
+++ same_topology.py
@@ -11,8 +11,8 @@ import os
 #Arguments for argparse module:
 parser = argparse.ArgumentParser(description = '''A program that filters entries in CATH
         according to a filter file containing PDB ids. All entries in each topology are then 
-	randomly paired and written to individual folders. One folder per topology.
-	Each toplogoical folder has subfolders, each with at most 100 pairs.''')
+	randomly paired, using one entry per H-group and written to individual folders. 
+	One folder per topology. Subfolders are created in batches of 200 entries within a topology.''')
 
 parser.add_argument('--H_groups', nargs=1, type= str,
                   default=sys.stdin, help = 'path to H_groups.')
@@ -95,7 +95,7 @@ def get_passed_uids(failed_pdb_filter, sequences, topologies):
             passed_uids.append(uid)
 
     #Group the passed uids by topology
-    passed_uids_grouped = {} #Sequences grouped by H-group
+    passed_uids_grouped = {} #Sequences grouped by topology
     for uid in passed_uids:
         topology = topologies[uid]
         if topology not in passed_uids_grouped.keys(): #If not in new dict - add
@@ -106,40 +106,43 @@ def get_passed_uids(failed_pdb_filter, sequences, topologies):
 
     return passed_uids_grouped
 
-def overx(passed_uids_grouped, outdir, sequences):
-    '''Write all uids to their respective topology in fasta format
-    '''
+def overx(passed_uids_grouped, outdir, sequences, H_groups):
+	'''Write all uids to their respective topology in fasta format
+	'''
 
-    #Write all passed groups to newline separated
-    #text files in partitions of 1000
-    topology_groupings = open('topology_groupings.txt', 'w')
+	#Write all passed groups to newline separated
+	#text files in partitions of 1000
+	topology_groupings = open('topology_groupings.txt', 'w')
 
-    x=2 #At least two entries per topology for a pair
-    #write passed_uids_grouped_over_x to fasta
-    for group in passed_uids_grouped:
-        group_dir = outdir+'fasta/'+group
-        uids = passed_uids_grouped[group]
-        if len(uids)<2:
-            continue
-        os.mkdir(group_dir)
-	#shuffle uids to make sure the groupings are random
-	random.Random(2).shuffle(uids)
-        for i in range(0,len(uids),200):
-            uids_200 = uids[i:i+200]
-            subgroup_dir = group_dir+'/'+str(i)
-            os.mkdir(subgroup_dir)
-            topology_groupings.write(subgroup_dir+'\n')		
-            for uid in uids_200:
-                sequence = sequences[uid]
-                with open(subgroup_dir+'/'+uid+'.fa', "w") as file:
-                    file.write('>'+uid+'\n')
-                    j = 0 #index
-                    while j<len(sequence):
-                        file.write(sequence[j:j+60]+'\n')
-                        j+=60
-    topology_groupings.close()
+	x=2 #At least two entries per topology for a pair
+	#write passed_uids_grouped_over_x to fasta
+	for group in passed_uids_grouped:
+		group_dir = outdir+'fasta/'+group
+		uids = passed_uids_grouped[group]
+		if len(uids)<2:
+			continue
+		os.mkdir(group_dir)
+		topology_groupings.write(subgroup_dir+'\n')
+		#shuffle uids to make sure the groupings are random
+		random.Random(2).shuffle(uids)
+		used_hgroups = [] #Save one uid per H-group
+		for uid in uids:
+			hgroup = H_groups[uid]
+			pdb.set_trace()
+			if hgroup in used_hgroups:#Only use each h-group once
+				continue
+			else:
+				used_hgroups.append(hgroup)
+			sequence = sequences[uid]
+			with open(subgroup_dir+'/'+uid+'.fa', "w") as file:
+				file.write('>'+uid+'\n')
+				j = 0 #index
+				while j<len(sequence):
+					file.write(sequence[j:j+60]+'\n')
+					j+=60
+	topology_groupings.close()
 
-    return None
+	return None
 
 #####MAIN#####
 args = parser.parse_args()
@@ -149,6 +152,5 @@ sequences = read_fasta(args.sequences[0]) #Read the clustered sequences
 failed_pdb_filter = pd.read_csv(args.failed_pdb_filter[0], sep = '\n', header = None) #Get uids that failed the pdb filter
 outdir = args.outdir[0]
 
-grouped_sequences = get_groups(topologies, sequences) #Group sequences
 passed_uids_grouped = get_passed_uids(failed_pdb_filter, sequences, topologies) #Get uids that passed the pdb filter and group them
-overx(passed_uids_grouped, outdir, sequences)
+overx(passed_uids_grouped, outdir, sequences, H_groups)
