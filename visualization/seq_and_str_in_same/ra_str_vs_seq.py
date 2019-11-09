82c43afb274fb9f4b567bf6126dd0562e213799d
patrickbryant1 <patrick.bryant@live.com>
2019-11-09 10:43:21 +0100

    plots topology and hgroup dataset in overlay

--- visualization/seq_and_str_in_same/ra_str_vs_seq.py
+++ visualization/seq_and_str_in_same/ra_str_vs_seq.py
@@ -16,7 +16,10 @@ import pdb
 #Arguments for argparse module:
 parser = argparse.ArgumentParser(description = '''A program that plots running averages.''')
 
-parser.add_argument('--df', nargs=1, type= str,
+parser.add_argument('--topdf', nargs=1, type= str,
+default=sys.stdin, help = 'path to df.')
+
+parser.add_argument('--hgroupdf', nargs=1, type= str,
 default=sys.stdin, help = 'path to df.')
 
 parser.add_argument('--outdir', nargs=1, type= str,
@@ -26,10 +29,11 @@ parser.add_argument('--calc', nargs=1, type= str,
 default=sys.stdin, help = 'either median or average.')
 
 
-def ra_different(df, aln_type, score, cardinality, calc, ylim, outdir):
+def ra_different(topdf, hgroupdf, aln_type, score, cardinality, calc, ylim, outdir):
     '''Produce running average plots for df
     '''
 
+    suffix = calc+'_'+score+cardinality+aln_type+'_'+'.svg'
     colors = {'_seqaln': 'k', '_straln': 'r'}
     xlabel = 'ML '+cardinality[1:]+' distance'
     grad_ylims = {'RMSD':[-0.1,0.1], 'lddt_scores':[-0.025, 0.025], 'DIFFSS':[-0.025, 0.025], 'DIFF_ACC':[-0.025, 0.025]}
@@ -45,9 +49,14 @@ def ra_different(df, aln_type, score, cardinality, calc, ylim, outdir):
     perc_points = []
     total_avs = {}
     step = 0.1
-    mldists = np.asarray(df['MLAAdist'+cardinality+aln_type])
-    scores = np.asarray(df[score+aln_type])
-
+    top_mldists = np.asarray(topdf['MLAAdist'+cardinality+aln_type])
+    top_scores = np.asarray(topdf[score+aln_type])
+    hgroup_mldists = np.asarray(hgroupdf['MLAAdist'+cardinality+aln_type])
+    hgroup_scores = np.asarray(hgroupdf[score+aln_type])
+    
+    mldists = np.append(top_mldists, hgroup_mldists)
+    scores = np.append(top_scores, hgroup_scores)
+    df = pd.concat([topdf, hgroupdf])
     fig = plt.figure(figsize=(10,10)) #set figsize
     for j in np.arange(min(mldists)+step,max(mldists)+step,step):
         below_df = df[df['MLAAdist'+cardinality+aln_type]<j]
@@ -55,63 +64,62 @@ def ra_different(df, aln_type, score, cardinality, calc, ylim, outdir):
         cut_scores = np.asarray(below_df[score+aln_type])
         if calc == 'average':
             av= np.average(cut_scores)
-        if calc == 'mean':
-            av= np.mean(cut_scores)
+        if calc == 'median':
+            av= np.median(cut_scores)
         avs.append(av)
         js.append(j-step/2)
         total_avs[j-step] = av
         perc_points.append(len(below_df)/len(df)*100)
 
-        #Include derivatives
-        grads = np.gradient(avs)
-        gradients[i] = grads
+    #Include derivatives
+    gradients = np.gradient(avs)
     #Plot RA
-    plt.plot(js, avs, linewidth = 1)
-    sizes = [js, perc_points] #Save percentage of points in each step
-    sns.kdeplot(mldists, scores,  shade=True, shade_lowest = False, cmap = 'Blues')
-    plt.title(score)
+    plt.plot(js, avs, linewidth = 2, c = 'b', label = 'Running average')
+    #sns.kdeplot(mldists, scores,  shade=True, shade_lowest = False, cmap = 'Blues')
+    plt.scatter(top_mldists, top_scores, s = 1, c = 'k', alpha = 1.0, label = 'topology')
+    plt.scatter(hgroup_mldists, hgroup_scores, s = 1, c = 'r', alpha = 0.2, label = 'hgroups')
     plt.xlabel(xlabel)
     plt.ylabel(score)
-    plt.ylim(ylim)
-    plt.xlim([0,6])
-    plt.xticks([0,1,2,3,4,5,6])
-    fig.savefig(outdir+'running_'+calc+'_'+score+'_'+cardinality+'_'+'.svg', format = 'svg')
+    plt.legend()
+    #plt.ylim(ylim)
+    plt.xlim([0,9])
+    plt.xticks([0,1,2,3,4,5,6,7,8,9])
+    fig.savefig(outdir+'running_'+suffix, format = 'svg')
 
 
     #Plot gradients
     fig = plt.figure(figsize=(10,10)) #set figsize
-    plt.scatter(sizes[i][0], gradients[i],s=2)
+    plt.scatter(js, gradients,s=5)
+    plt.plot(js, gradients, linewidth = 1)
     plt.ylabel('gradient')
-    plt.ylim(grad_ylims[score])
-    plt.xlim([0,6])
-    plt.xticks([0,1,2,3,4,5,6])
-    fig.savefig(outdir+'gradient_running_'+calc+'_'+score+'_'+cardinality+'_'+'.svg', format = 'svg')
+    #plt.ylim(grad_ylims[score])
+    #plt.xlim([2,10])
+    #plt.xticks([2,3,4,5,6,7,8,9,10])
+    fig.savefig(outdir+'gradient_running_'+suffix, format = 'svg')
     #Plot Point distribution
     fig = plt.figure(figsize=(10,10)) #set figsize
-    plt.plot(sizes[i][0], sizes[i][1], linewidth = 1)
+    plt.plot(js, perc_points, linewidth = 1)
     plt.xlabel(xlabel)
     plt.ylabel('% of points')
-    plt.legend(loc = 'best')
-    plt.ylim([0,20])
-    plt.xlim([0,6])
-    plt.xticks([0,1,2,3,4,5,6])
-    fig.savefig(outdir+'perc_points_running_'+calc+'_'+score+'_'+cardinality+'_'+'.svg', format = 'svg')
+    #plt.xlim([2,10])
+    #plt.xticks([2,3,4,5,6,7,8,9,10])
+    fig.savefig(outdir+'perc_points_running_'+suffix, format = 'svg')
 
     return None
 
 
 #####MAIN#####
 args = parser.parse_args()
-df = pd.read_csv(args.df[0])
+topdf = pd.read_csv(args.topdf[0])
+hgroupdf = pd.read_csv(args.hgroupdf[0])
 outdir = args.outdir[0]
 calc = args.calc[0]
 
-
 aln_types = ['_seqaln', '_straln']
-ylims = {'RMSD':[0,4], 'DIFFSS':[0, 0.6], 'DIFF_ACC':[0,0.6], 'lddt_scores': [0.5,1.0]}
+ylims = {'RMSD':[0,4], 'DIFFSS':[0, 0.6], 'DIFF_ACC':[0,0.6], 'lddt_scores': [0.35,0.75]}
 
 cardinality = '_AA20'
 for score in ['RMSD','DIFFSS', 'DIFF_ACC', 'lddt_scores']:
     for aln_type in aln_types:
         ylim = ylims[score]
-        ra_different(df, aln_type, score, cardinality, calc, ylim, outdir)
+        ra_different(topdf, hgroupdf, aln_type, score, cardinality, calc, ylim, outdir)
