c9ab57233a2bb97f849824160be3392b11fad5f7
Patrick <pbryant@ae18a.scilifelab.se>
2020-01-08 13:43:54 +0100

    parsing old alignments and making phylip files

--- SCOP_comparison/parse_old.py
+++ SCOP_comparison/parse_old.py
@@ -2,26 +2,60 @@
 # -*- coding: utf-8 -*-
 
 
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-from matplotlib.backends.backend_pdf import PdfPages
 import pandas as pd
 from collections import Counter
 import numpy as np
-import seaborn as sns
 import sys
 import os
 import argparse
-from scipy import ndimage
-
 import pdb
+
+#custom imports
+sys.path.insert(1, '/home/pbryant/evolutionary_rates')
+from conversions import make_phylip
 
 #Arguments for argparse module:
 parser = argparse.ArgumentParser(description = '''A program that parses the 2009 alignments and returns
                                                 the SCOP identifiers as well as alignments for tree-puzzle.''')
 
 parser.add_argument('--infile', nargs=1, type= str, default=sys.stdin, help = 'path to infile.')
+
+#####FUNCTIONS#####
+def parse(infile):
+    '''A function that parses the alignments from the 2009 dataset
+    Illergård, K., Ardell, D. H. & Elofsson, A. Structure is three to ten times more conserved than sequence-
+    A study of structural response in protein cores.
+    Proteins: Structure, Function, and Bioinformatics vol. 77 499–508 (2009).
+    '''
+
+    #Save information about ids and alignments
+    uid1 = []
+    uid2 = []
+    alignments = [[], []] #refers to alignments of all AA20 (all standard amino acids)
+
+    with open(infile, 'r') as f:
+        get_next = False #Keep track of alignments fetched
+        for line in f:
+            line = line.rstrip()
+            if '= vs =' in line:
+                line = line.split('=')
+                uid1.append(line[13].strip().rstrip('_'))
+                uid2.append(line[23].strip().rstrip('_'))
+            if '>Ali1_AA' == line:
+                get_next = True
+                index = 0
+                continue
+
+            if '>Ali2_AA' == line:
+                get_next = True
+                index = 1
+                continue
+
+            if get_next == True:
+                alignments[index].append(line)
+                get_next = False
+
+        pdb.set_trace()
 
 
 
